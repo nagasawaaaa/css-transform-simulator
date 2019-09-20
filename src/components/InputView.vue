686a2ccdbb5ce43ab1f2bb98026d2e2333d4d7d0
長澤 賢 <nagasawa@pensees.co.jp>
2019-09-21 00:11:16 +0900

    first commit

---
+++ src/components/InputView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="input">
+    <b-row>
+      <b-col cols="4">
+        <label :for="`type-${id}`">{{ label }}</label>
+        :{{ value }}
+      </b-col>
+      <b-col cols="8">
+        <b-form-input
+          :id="`type-${id}`"
+          v-model="value"
+          type="range"
+          :min="min"
+          :max="max"
+          @input="input"
+        ></b-form-input>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputView",
+  data() {
+    return {
+      value: 0
+    };
+  },
+  props: {
+    id: {
+      type: String,
+      default: "labelName"
+    },
+    label: {
+      type: String,
+      default: "label Name"
+    },
+    max: {
+      type: Number,
+      default: 1000
+    },
+    min: {
+      type: Number,
+      default: -1000
+    }
+  },
+  methods: {
+    input() {
+      this.$emit("input", this.value);
+    }
+  }
+};
+</script>
---
+++ src/components/LayoutView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wrapper">
+    <b-container>
+      <h2>CSS Transform Simulator</h2>
+      <b-row>
+        <b-col cols="6">
+          <InputView
+            id="translateX"
+            label="translateX"
+            @input="getTransformValue($event, 'translateX')"
+          />
+          <InputView
+            id="translateY"
+            label="translateY"
+            @input="getTransformValue($event, 'translateY')"
+          />
+          <InputView
+            id="translateZ"
+            label="translateZ"
+            @input="getTransformValue($event, 'translateZ')"
+          />
+        </b-col>
+        <b-col cols="6">col-8</b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import InputView from "./InputView";
+
+export default {
+  name: "LayoutView",
+  data() {
+    return {
+      transform: {
+        translateX: 0,
+        translateY: 0,
+        translateZ: 0
+      }
+    };
+  },
+  components: {
+    InputView
+  },
+  methods: {
+    getTransformValue(payload, property) {
+      this.transform[property] = payload;
+    }
+  }
+};
+</script>
