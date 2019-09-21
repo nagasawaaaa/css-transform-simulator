bf75babfee40cc1536195cb5214db4570142f87e
長澤 賢 <nagasawa@pensees.co.jp>
2019-09-21 16:58:19 +0900

    add: CssPreview

---
+++ src/components/CssPreview.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="cssPreview">
+    <pre>
+      <code>
+.hoge {
+  transform: {{ createTranslate(transform) }}
+             {{ createRotate3d(transform) }}
+}
+      </code>
+    </pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "cssPreview",
+  props: {
+    transform: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+  methods: {
+    createTranslate(transform) {
+      return `translate3d(${transform.translateX}px, ${transform.translateY}px, ${transform.translateZ}px)`;
+    },
+    createRotate3d(transform) {
+      return `rotateX(${transform.rotateX}deg) rotateY(${transform.rotateY}deg) rotateZ(${transform.rotateZ}deg)`;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.cssPreview {
+  background-color: #efefef;
+  font-size: 14px;
+  min-height: 50vh;
+  padding: 16px;
+}
+</style>
--- src/components/LayoutView.vue
+++ src/components/LayoutView.vue
@@ -5,24 +5,53 @@
       <b-row>
         <b-col cols="6">
           <h3 class="mb-4">Property Settings</h3>
-          <InputView
-            id="translateX"
-            label="translateX"
-            @input="getTransformValue($event, 'translateX')"
-          />
-          <InputView
-            id="translateY"
-            label="translateY"
-            @input="getTransformValue($event, 'translateY')"
-          />
-          <InputView
-            id="translateZ"
-            label="translateZ"
-            @input="getTransformValue($event, 'translateZ')"
-          />
+          <section class="mb-4">
+            <h4>translate</h4>
+            <InputView
+              id="translateX"
+              label="translateX"
+              @input="getTransformValue($event, 'translateX')"
+            />
+            <InputView
+              id="translateY"
+              label="translateY"
+              @input="getTransformValue($event, 'translateY')"
+            />
+            <InputView
+              id="translateZ"
+              label="translateZ"
+              @input="getTransformValue($event, 'translateZ')"
+            />
+          </section>
+          <section>
+            <h4>rotate</h4>
+            <InputView
+              id="rotateX"
+              label="rotateX"
+              :max="360"
+              :min="-360"
+              @input="getTransformValue($event, 'rotateX')"
+            />
+            <InputView
+              id="rotateY"
+              label="rotateY"
+              :max="360"
+              :min="-360"
+              @input="getTransformValue($event, 'rotateY')"
+            />
+            <InputView
+              id="rotateZ"
+              label="rotateZ"
+              :max="360"
+              :min="-360"
+              @input="getTransformValue($event, 'rotateZ')"
+            />
+          </section>
         </b-col>
         <!-- TODO: プレビュー用のViewとCSSのViewを作成する -->
-        <b-col cols="6">col-8</b-col>
+        <b-col cols="6">
+          <CssPreview :transform="transform" />
+        </b-col>
       </b-row>
     </b-container>
   </div>
@@ -30,6 +59,7 @@
 
 <script>
 import InputView from "./InputView";
+import CssPreview from "./CssPreview";
 
 export default {
   name: "LayoutView",
@@ -38,12 +68,18 @@ export default {
       transform: {
         translateX: 0,
         translateY: 0,
-        translateZ: 0
+        translateZ: 0,
+        rotateX: 0,
+        rotateY: 0,
+        rotateZ: 0,
+        scaleX: 0,
+        scaleY: 0
       }
     };
   },
   components: {
-    InputView
+    InputView,
+    CssPreview
   },
   methods: {
     getTransformValue(payload, property) {
