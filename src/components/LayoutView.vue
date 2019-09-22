be5db579fb06e5ff32dd887e2d1a619fdb34457f
nagasawaaaa <snagasaw.sp.create@gmail.com>
2019-09-22 23:06:33 +0900

    update: footer link

--- src/components/LayoutView.vue
+++ src/components/LayoutView.vue
@@ -26,7 +26,7 @@
         </b-col>
       </b-row>
       <p class="text-center">
-        &copy; nagasawaaaa All Rights Reserved.
+        &copy; <a href="https://twitter.com/nagasawaaaa" target="_blank">nagasawaaaa</a> All Rights Reserved.
       </p>
     </b-container>
   </div>
